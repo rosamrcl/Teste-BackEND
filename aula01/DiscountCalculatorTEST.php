7403d2dacd3bd66e236f38fb1b9c9e16d048ddae
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-07-08 15:53:20 -0300

    .

---
+++ aula01/DiscountCalculatorTEST.php
@@ -0,0 +1,21 @@
+<?php
+class DiscountCalculatorTEST{
+    public function ShouldApply_WhenValueIsAboveMinimumTest(){
+        $discountCalculator = new DiscountCalculator();
+
+        $totalValue = 130;
+        $totalWithdiscount = $discountCalculator->apply(value:$totalValue);
+
+        $expectedValue =110;
+        $this->asserEquals($expectedValue, $totalWithdiscount);
+    }
+    public function asserEquals($expectedValue, $actualValue){
+        if ($expectedValue===$actualValue){
+            $message = 'Expected: ' . $expectedValue . 'but got: '.$actualValue;
+            throw new Exception(message:$message);
+        }
+        echo "Test passed! \n";
+    }
+}
+
+?>
