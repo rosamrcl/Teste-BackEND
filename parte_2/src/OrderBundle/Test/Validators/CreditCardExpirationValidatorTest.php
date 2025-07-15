15ed7929ae426fe9057fb15b4938eb1347e544eb
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-07-15 15:13:52 -0300

    .

--- parte_2/src/OrderBundle/Test/Validators/CreditCardExpirationValidatorTest.php
+++ parte_2/src/OrderBundle/Test/Validators/CreditCardExpirationValidatorTest.php
@@ -1,3 +1,23 @@
 <?php
 
+namespace OrderBundle\Test\Validators;
+
+use PHPUnit\Framework\TestCase;
+use OrderBundle\Validators\CreditCardExpirationValidator;
+
+class CreditCardExpirationValidatorTest extends TestCase{
+    public function testExpirationDateShouldBeValid(){
+        $futureDate = "12/12/2025";        
+        $validator = new CreditCardExpirationValidator($futureDate);
+        $isValid = $validator ->isValid();
+        $this->assertTrue($isValid);
+    }
+    public function testExpirationDateShouldBeInvalidWhenInPast() {
+        $pastDate = "12/12/2020";
+        $validator = new CreditCardExpirationValidator($pastDate);
+        
+        $this->assertFalse($validator->isValid());
+    }
+}
+
 ?>
--- parte_2/src/OrderBundle/Test/Validators/CreditCardNumberValidatorTest.php
+++ parte_2/src/OrderBundle/Test/Validators/CreditCardNumberValidatorTest.php
@@ -1,3 +1,22 @@
 <?php
+namespace OrderBundle\Test\Validators;
 
+use PHPUnit\Framework\TestCase;
+use OrderBundle\Validators\CreditCardNumberValidator;
+
+
+class CreditCardNumberValidatorTest extends testCase{
+    public function testNumberValidator(){
+        $credit = "1234567891011121";
+        $creditnumber = new CreditCardNumberValidator($credit);
+        $isValid = $creditnumber ->isValid();
+        $this->assertTrue($isValid);
+    }
+    public function testNotNumberValidator(){
+        $credit = "a";
+        $creditnumber = new CreditCardNumberValidator($credit);
+        $isValid = $creditnumber ->isValid();
+        $this->assertfalse($isValid);
+    }
+}
 ?>
